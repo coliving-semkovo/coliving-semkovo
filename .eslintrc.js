6bd03621aaa29d1f6b4406f143f11445290dc252
John Boyes <john@agilepathway.co.uk>
2024-03-05 15:43:45 +0000

    chore: ignore ESLint errors reported in Vercel
    We can safely ignore these errors as we are using Biome instead.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,10 @@ module.exports = {
 	plugins: ["simple-import-sort", "@typescript-eslint"],
 	rules: {
 		"sort-imports": "off",
+		// turned off mixed spaces and tabs checking as it was conflicting with Biome,
+		// because of ternary handling it looks like:
+		// https://github.com/biomejs/biome/issues/1661
+		"no-mixed-spaces-and-tabs": "off",
 		"tailwindcss/no-custom-classname": "off",
 		"@typescript-eslint/no-var-requires": "off",
 		"simple-import-sort/imports": [
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
+/* eslint-disable no-var */
 declare global {
-	// biome-ignore lint/style/noVar: ESLint was also ignoring this line
+	// biome-ignore lint/style/noVar: This rule is also disabled for ESLint. Will investigate later
 	var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
 // biome-ignore lint/suspicious/noRedeclare: to be investigated later
 const prisma = global.prisma || new PrismaClient();
