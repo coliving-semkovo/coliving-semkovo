c0dae2fe5034b766f2cb459d1ad36d019ff3ff77
John Boyes <john@agilepathway.co.uk>
2024-03-05 07:03:49 +0000

    chore: remove Prettier as we are now using Biome

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,12 @@ module.exports = {
 	parser: "@typescript-eslint/parser",
 	extends: [
 		"next/core-web-vitals",
-		"prettier",
 		"eslint:recommended",
-		"plugin:prettier/recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:tailwindcss/recommended",
 	],
-	plugins: ["simple-import-sort", "prettier", "@typescript-eslint"],
+	plugins: ["simple-import-sort", "@typescript-eslint"],
 	rules: {
-		"prettier/prettier": [
-			"error",
-			{
-				endOfLine: "auto",
-			},
-		],
 		"sort-imports": "off",
 		"tailwindcss/no-custom-classname": "off",
 		"@typescript-eslint/no-var-requires": "off",
