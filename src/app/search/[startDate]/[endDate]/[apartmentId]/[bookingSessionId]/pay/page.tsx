e3618721a5f5df15240e7522f813690df4e48915
Toby Urff <38977205+tobyurff@users.noreply.github.com>
2024-03-08 08:15:08 +0000

    feat: add booking funnel /pay page to go through payment confirmation

---
+++ src/app/search/[startDate]/[endDate]/[apartmentId]/[bookingSessionId]/pay/page.tsx
@@ -0,0 +1,19 @@
+import WireframeSection from "@/components/wireframe/WireframeSection";
+
+const BookingFunnelPaymentDetails = () => {
+	return (
+		<main>
+			<WireframeSection
+				title="Payment form"
+				explanation="This page will enable future community members to choose their preferred payment method, focusing on cost-effective and low-administration options like GoCardless or Stripe's non-card methods. This will involve redirecting them to their financial institution for payment confirmation."
+				imageUrl="/wireframe/bank-payment.png"
+				links={{
+					route: "/stays/83094882939543",
+					text: "Next, let's look at what happens after they have successfully completed the payment, here and at their financial institution, and we can confirm their booking.",
+				}}
+			/>
+		</main>
+	);
+};
+
+export default BookingFunnelPaymentDetails;
