01314cc3ed54c4d7e60fcba1a2bebed431b8689a
John Boyes <john@agilepathway.co.uk>
2024-04-03 07:34:02 +0000

    chore: error if server check imported on frontend
    Added this as per the suggestion in
    [this article][1].
    
    [1]: https://sophiabits.com/blog/verify-environment-variables-nextjs

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,12 @@
 // @ts-check
 const { z } = require("zod");
 
+if (typeof window !== "undefined") {
+	throw new Error(
+		"src/server/config.js must not be imported on the frontend (as it validates server-side vars).",
+	);
+}
+
 const Config = z.object({
 	DATABASE_URL: z.string(),
 });
