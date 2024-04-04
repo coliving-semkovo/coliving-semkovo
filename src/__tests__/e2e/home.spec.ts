2efb0a9de20e54d5a4750c770917aa4d0fcb4d19
John Boyes <john@agilepathway.co.uk>
2024-04-04 06:40:29 +0000

    test: first go at adding the x header

--- src/__tests__/e2e/home.spec.ts
+++ src/__tests__/e2e/home.spec.ts
@@ -1,5 +1,13 @@
 import { expect, test } from "@playwright/test";
 
+test.beforeEach(async ({ page }, testInfo) => {
+	await page.route("**/*", async (route) => {
+		const headers = route.request().headers();
+		headers["X-Test-DB-Schema"] = testInfo.testId;
+		await route.continue({ headers });
+	});
+});
+
 test("has title", async ({ page }) => {
 	await page.goto("/");
 
