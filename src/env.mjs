05f08381bb532847cbd90d37aab095f61c5ee8f5
John Boyes <john@agilepathway.co.uk>
2024-04-07 12:05:50 +0300

    chore: add checkjs error reporting for js files (#29)
    Having error reporting for js files is a good thing, so good to enable
    this via [checkjs][1].
    The `env.mjs` file failed the error checking, complaining that the
    vars prefixed with `NEXT_PUBLIC_` should not have this prefix, for some
    reason. So we exclude those lines from the error checking for now, and
    will investigate and hopefully fix them at a later point.
    
    [1]: https://www.typescriptlang.org/tsconfig#checkJs

--- src/env.mjs
+++ src/env.mjs
@@ -3,9 +3,13 @@ import { z } from "zod";
 
 export const env = createEnv({
 	server: {
+		// @ts-ignore: TODO: investigate and fix the TypeScript check error
 		NEXT_PUBLIC_SITE_URL: z.string().url().optional(),
+		// @ts-ignore: TODO: investigate and fix the TypeScript check error
 		NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_ID: z.string().min(1).optional(),
+		// @ts-ignore: TODO: investigate and fix the TypeScript check error
 		NEXT_PUBLIC_GITHUB_ID: z.string().min(1).optional(),
+		// @ts-ignore: TODO: investigate and fix the TypeScript check error
 		NEXT_PUBLIC_GITHUB_SECRET: z.string().min(1).optional(),
 	},
 	runtimeEnv: {
