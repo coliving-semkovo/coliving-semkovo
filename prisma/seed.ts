82cad1f98a09869425136b4861dc2e5dbaa20962
John Boyes <john@agilepathway.co.uk>
2024-04-16 10:02:51 +0000

    chore: add script to seed dummy data

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import prisma from "./../src/lib/prisma";
+
+async function main() {
+	const alice = await prisma.user.upsert({
+		where: { email: "alice@prisma.io" },
+		update: {},
+		create: {
+			email: "alice@prisma.io",
+			name: "Alice",
+		},
+	});
+	const bob = await prisma.user.upsert({
+		where: { email: "bob@prisma.io" },
+		update: {},
+		create: {
+			email: "bob@prisma.io",
+			name: "Bob",
+		},
+	});
+	console.log({ alice, bob });
+}
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
