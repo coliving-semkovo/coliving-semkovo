8793a290f7cb3a5c5cb0114aefeea7b8e0c78b70
John Boyes <john@agilepathway.co.uk>
2024-04-17 13:54:13 +0000

    refactor: extract method

--- src/__tests__/e2e/lib/prisma.ts
+++ src/__tests__/e2e/lib/prisma.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-export default function prismaClient(dbSchema: string): PrismaClient {
+export function dbURL(dbSchema: string): URL {
 	if (!process.env.DATABASE_URL) {
 		throw new Error("please provide a database url");
 	}
 	const dbURL = new URL(process.env.DATABASE_URL);
 	dbURL.searchParams.set("schema", dbSchema);
+	return dbURL;
+}
+
+export default function prismaClient(dbSchema: string): PrismaClient {
 	return new PrismaClient({
-		datasourceUrl: dbURL.toString(),
+		datasourceUrl: dbURL(dbSchema).toString(),
 	});
 }
