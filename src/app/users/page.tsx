619234a4b538e3de98aeb66f86044a0377ed0cd5
John Boyes <john@agilepathway.co.uk>
2024-04-17 01:49:46 +0000

    chore: add users page

---
+++ src/app/users/page.tsx
@@ -0,0 +1,16 @@
+import prisma from "../../lib/prisma";
+
+export default async function Users() {
+	const users = await prisma.user.findMany();
+
+	return (
+		<main>
+			<h1>Users</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</main>
+	);
+}
