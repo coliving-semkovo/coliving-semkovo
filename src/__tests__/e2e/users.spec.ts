6e75a855b56fb2645523eeaca653948c77c8f886
John Boyes <john@agilepathway.co.uk>
2024-04-17 05:44:58 +0000

    chore: add second users test

--- src/__tests__/e2e/users.spec.ts
+++ src/__tests__/e2e/users.spec.ts
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page }, testInfo) => {
 	});
 });
 
-test("has Jim and Jane", async ({ page }) => {
+test("has just Jim and Jane", async ({ page }) => {
 	await prisma.user.upsert({
 		where: { email: "jim@prisma.io" },
 		update: {},
@@ -31,4 +31,34 @@ test("has Jim and Jane", async ({ page }) => {
 
 	await expect(page.getByText("Jim")).toBeVisible();
 	await expect(page.getByText("Jane")).toBeVisible();
+
+	const usersList = page.getByRole("listitem");
+	await expect(usersList).toHaveCount(2);
+});
+
+test("has just Alice and Bob", async ({ page }) => {
+	await prisma.user.upsert({
+		where: { email: "alice@prisma.io" },
+		update: {},
+		create: {
+			email: "alice@prisma.io",
+			name: "Alice",
+		},
+	});
+	await prisma.user.upsert({
+		where: { email: "bob@prisma.io" },
+		update: {},
+		create: {
+			email: "bob@prisma.io",
+			name: "Bob",
+		},
+	});
+
+	await page.goto("/users");
+
+	await expect(page.getByText("Alice")).toBeVisible();
+	await expect(page.getByText("Bob")).toBeVisible();
+
+	const usersList = page.getByRole("listitem");
+	await expect(usersList).toHaveCount(2);
 });
