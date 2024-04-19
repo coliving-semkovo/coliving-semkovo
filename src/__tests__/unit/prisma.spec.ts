1f3cd42cdecfbf7362bce6fb540ad0bb49e34c75
John Boyes <john@agilepathway.co.uk>
2024-04-19 07:34:01 +0000

    chore: an improvement

--- src/__tests__/unit/prisma.spec.ts
+++ src/__tests__/unit/prisma.spec.ts
@@ -1,22 +1,26 @@
 import prisma from "../../lib/prisma";
 
+const mockHeaders = {
+	get: jest.fn().mockImplementation(() => {
+		return "mydb-schema";
+	}),
+	has: jest.fn().mockImplementation(() => {
+		return true;
+	}),
+};
+
 jest.mock("next/headers", () => {
 	return {
 		headers: () => {
-			return {
-				get: jest.fn().mockImplementation(() => {
-					return "mydb-schema";
-				}),
-				has: jest.fn().mockImplementation(() => {
-					return true;
-				}),
-			};
+			return mockHeaders;
 		},
 	};
 });
 
 describe("Prisma", () => {
-	test("something", () => {
+	test("sets the DB schema from the request header if present", () => {
 		prisma();
+		expect(mockHeaders.has).toHaveBeenCalledWith("X-Test-DB-Schema");
+		expect(mockHeaders.get).toHaveBeenCalledWith("X-Test-DB-Schema");
 	});
 });
